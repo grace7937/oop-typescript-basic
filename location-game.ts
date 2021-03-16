6d0919f8c1c32a0cf3d589c4789534771404614c
김은총 <eunchong@gim-eunchong-ui-MacBookAir.local>
2021-03-16 19:48:10 +0900

    location-game

---
+++ location-game.ts
@@ -0,0 +1,23 @@
+
+
+const positon = { x: 0, y: 0 };
+
+function move(direction: 'up' | 'down' | 'left' | 'right') {
+    switch(direction) {
+        case 'up' : 
+           position.y +=1;
+            break;
+        case 'left' : 
+            x + 1;
+            break;
+        //이렇게 down, left까지 작성한다.
+    }
+}
+
+
+
+console.log(position);  // { x: 0, y: 0}
+move('up');
+move('down');
+move('left');
+move('right');
