488674769421daf350d898ae0d71c407e2935940
김은총 <eunchong.k153@gmail.com>
2021-04-29 20:52:32 +0900

    Add 1-4-aliases.ts

---
+++ 1-4-aliases.ts
@@ -0,0 +1,26 @@
+{
+  //Type Aliases 란 새로운 타입을 내가 정의 하는것! TypeScript의 꽃🌹
+  type Text = string;
+  // const name: string = 'ana';  //또는
+  const name: Text = "ana";
+
+  type Num = number;
+  type Student = {
+    name: string;
+    age: number;
+  };
+
+  const student: Student = {
+    name: "ellie",
+    age: 15,
+  };
+
+
+  
+  //String Literal Types
+  //위에서는 Type에 Type을 할당해 줬지만 이것은 그냥 글자 그자체를 할당한다.
+  type Name = 'name';
+  let eName: Name;
+  eName = 'name';        //eName에는 'name'이라는 글자밖에 못쓴다.
+  
+}
